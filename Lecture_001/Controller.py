b273f6ebba847664fa642a60d70a9f7b84af8f14
Holllmess <96007270+Holllmess@users.noreply.github.com>
2022-05-25 16:50:41 +0300

    Delete Lecture_001 directory

--- Lecture_001/Controller.py
+++
@@ -1,9 +0,0 @@
-import Model_sub as Model
-import View
-
-def button_click(): 
-  value_a = View.get_value()
-  value_b = View.get_value()
-  Model.init(value_a, value_b)
-  result = Model.do_it()
-  View.view_data(result)
